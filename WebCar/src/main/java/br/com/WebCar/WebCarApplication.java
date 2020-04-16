96ed2d93ab5bdcd00aa233448c29e9cd8ce7ecfb
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2020-04-16 00:52:13 -0300

    Estrutura

---
+++ WebCar/src/main/java/br/com/WebCar/WebCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.WebCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCarApplication.class, args);
+	}
+
+}
---
+++ WebCar/src/test/java/br/com/WebCar/WebCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.WebCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
