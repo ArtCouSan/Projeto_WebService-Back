7cad7480f8ebee9b20d539e0f006cbe6671f847a
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2020-05-10 23:46:42 -0300

    Ajustado save user

--- WebCar/src/main/resources/db/carga.sql
+++ WebCar/src/main/resources/db/carga.sql
@@ -1,22 +1,22 @@
 INSERT INTO tb_user
-(user_name, user_birth, user_cpf, user_status, user_in_vehicle)
+(user_name, user_birth, user_cpf, user_status, user_in_vehicle, user_password)
  VALUES
- ('Arthur Coutinho Santos','1988-10-02', '000.000.000-00', true, false);
+ ('Arthur Coutinho Santos','1988-10-02', '000.000.000-00', true, false, 'pastel');
 
 INSERT INTO tb_user
-(user_name, user_birth, user_cpf, user_status, user_in_vehicle)
+(user_name, user_birth, user_cpf, user_status, user_in_vehicle, user_password)
  VALUES
- ('Renan','1988-10-02', '000.000.000-02', true, false);
+ ('Renan','1988-10-02', '000.000.000-02', true, false, 'pastel');
 
 INSERT INTO tb_user
-(user_name, user_birth, user_cpf, user_status, user_in_vehicle)
+(user_name, user_birth, user_cpf, user_status, user_in_vehicle, user_password)
  VALUES
- ('Rubens','1988-10-02', '000.000.000-03', true, false);
+ ('Rubens','1988-10-02', '000.000.000-03', true, false, 'pastel');
 
 INSERT INTO tb_user
-(user_name, user_birth, user_cpf, user_status, user_in_vehicle)
+(user_name, user_birth, user_cpf, user_status, user_in_vehicle, user_password)
  VALUES
- ('Eric','1988-10-02', '000.000.000-04', true, false);
+ ('Eric','1988-10-02', '000.000.000-04', true, , 'pastel');
 
 INSERT INTO tb_vehicle
 (vehicle_plate, vehicle_nick, vehicle_model, vehicle_year, vehicle_color, vehicle_status, vehicle_mileage)
